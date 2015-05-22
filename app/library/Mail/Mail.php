55f605269d781e4d0bb9133f76871d85fc412eaa
andremfagundes <amfcom@gmail.com>
2015-05-22 16:10:59 -0300

    Verificar erro de envio de email.

--- app/library/Mail/Mail.php
+++ app/library/Mail/Mail.php
@@ -126,7 +126,7 @@ class Mail extends Component
 
         // Settings
         $mailSettings = $this->config->mail;
-die($mailSettings.'**');
+die('**');
         $template = $this->getTemplate($nome, $params);
 
         // Create the message
